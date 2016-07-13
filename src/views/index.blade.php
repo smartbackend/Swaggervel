ea0a8f691afd891115a6ed91a6be809d50d198a0
smartbackend <admin@smartbackend.com>
2016-07-14 02:52:45 +0800

    upgrade swagger ui

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -12,20 +12,19 @@ header("Access-Control-Allow-Headers: X-Requested-With");
         <title>Swagger UI</title>
         <link rel="icon" type="image/png" href="/vendor/swaggervel/images/favicon-32x32.png" sizes="32x32" />
         <link rel="icon" type="image/png" href="/vendor/swaggervel/images/favicon-16x16.png" sizes="16x16" />
-        <link href='/vendor/swaggervel/css/typography.css' media='screen' rel='stylesheet' type='text/css'/>
-        <link href='/vendor/swaggervel/css/reset.css' media='screen' rel='stylesheet' type='text/css'/>
-        <link href='/vendor/swaggervel/css/screen.css' media='screen' rel='stylesheet' type='text/css'/>
-        <link href='/vendor/swaggervel/css/reset.css' media='print' rel='stylesheet' type='text/css'/>
-        <link href='/vendor/swaggervel/css/print.css' media='print' rel='stylesheet' type='text/css'/>
+        <script src='/vendor/swaggervel/lib/object-assign-pollyfill.js' type='text/javascript'></script>
         <script src='/vendor/swaggervel/lib/jquery-1.8.0.min.js' type='text/javascript'></script>
         <script src='/vendor/swaggervel/lib/jquery.slideto.min.js' type='text/javascript'></script>
         <script src='/vendor/swaggervel/lib/jquery.wiggle.min.js' type='text/javascript'></script>
         <script src='/vendor/swaggervel/lib/jquery.ba-bbq.min.js' type='text/javascript'></script>
         <script src='/vendor/swaggervel/lib/handlebars-2.0.0.js' type='text/javascript'></script>
-        <script src='/vendor/swaggervel/lib/underscore-min.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/js-yaml.min.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/lodash.min.js' type='text/javascript'></script>
         <script src='/vendor/swaggervel/lib/backbone-min.js' type='text/javascript'></script>
         <script src='/vendor/swaggervel/swagger-ui.js' type='text/javascript'></script>
-        <script src='/vendor/swaggervel/lib/highlight.7.3.pack.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/highlight.9.1.0.pack.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/highlight.9.1.0.pack_extended.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/jsoneditor.min.js' type='text/javascript'></script>
         <script src='/vendor/swaggervel/lib/marked.js' type='text/javascript'></script>
 
         <script src='/vendor/swaggervel/lib/swagger-oauth.js' type='text/javascript'></script>
